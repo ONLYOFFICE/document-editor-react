59f949690d6ed875b526e781d64109afa747502b
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2024-01-31 12:12:01 +0300

    build: bump deps for storybook

--- src/DocumentEditor.stories.tsx
+++ src/DocumentEditor.stories.tsx
@@ -24,6 +24,7 @@ export default {
   title: "DocumentEditor",
   component: DocumentEditor,
   parameters: {},
+  tags: ["autodocs"],
   decorators: [
     (Story) => (
       <div style={{ height: "400px" }}>
